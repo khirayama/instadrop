b0e355a6d5b2c3eef360290c0cca791094b596ae
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-11-04 20:27:04 +0900

    build: set up typescript env for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
