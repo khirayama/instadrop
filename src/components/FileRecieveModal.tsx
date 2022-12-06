4c3cc2c2dd8571029a623a0b547e4a8deac60f99
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2022-12-06 21:34:53 +0900

    refactor: apply eslint to FileRecieveModal

--- src/components/FileRecieveModal.tsx
+++ src/components/FileRecieveModal.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import {
   Button,
   Box,
@@ -10,31 +11,31 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
+  ModalCloseButton
 } from '@chakra-ui/react'
-import i18next from 'i18next';
-import { Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js'; // TODO
+import i18next from 'i18next'
+import { Navigation, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js' // TODO
 
 import 'swiper/swiper-bundle.min.css'
 import 'swiper/swiper.min.css'
 
-const t = i18next.t;
+const t = i18next.t
 
-type FileRecieveModalProps = {
-  files: any[]; // TODO
-  disclosure: any; // TODO
-};
+interface FileRecieveModalProps {
+  files: any[] // TODO
+  disclosure: any // TODO
+}
 
-export function FileRecieveModal(props: FileRecieveModalProps) {
-  const dc = props.disclosure;
-  const files = props.files;
+export function FileRecieveModal (props: FileRecieveModalProps): JSX.Element {
+  const dc = props.disclosure
+  const files = props.files
 
   return (
     <Modal onClose={dc.onClose} isOpen={dc.isOpen} isCentered>
       <ModalOverlay />
       <ModalContent maxW="88%">
-        <ModalHeader>{t('sharedFilesMessage', { count: files.length })}</ModalHeader>
+        <ModalHeader>{t('sharedFileMessage', { count: files.length })}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           <Swiper
@@ -43,52 +44,44 @@ export function FileRecieveModal(props: FileRecieveModalProps) {
           >
             {
               files
-              .map((f) => {
-                const obj = window.URL.createObjectURL(f);
-                let preview = null;
-                if (f.type.indexOf('image') == 0) {
-                  preview = (
+                .map((f) => {
+                  const obj = window.URL.createObjectURL(f)
+                  let preview = null
+                  if (f.type.indexOf('image') === 0) {
+                    preview = (
                     <Box>
                       <Image src={obj} />
                     </Box>
-                  );
-                } else if (f.type.indexOf('text') == 0) {
-                  if (!files[f.name]) {
-                    f.text().then((res) => {
-                      const fs = {
-                        ...files,
-                        [f.name]: res,
-                      };
-                    });
-                  }
-                  preview = (
+                    )
+                  } else if (f.type.indexOf('text') === 0) {
+                    preview = (
                     <Box>
                       <Text>{f.name}</Text>;
                       <Code>{files[f.name]}</Code>
                     </Box>
-                  );
-                } else {
-                  preview = <Box>{f.name}</Box>;
-                }
-                return (
+                    )
+                  } else {
+                    preview = <Box>{f.name}</Box>
+                  }
+                  return (
                   <SwiperSlide key={f.name}>
-                    <Box style={{height: '100%'}}>
+                    <Box style={{ height: '100%' }}>
                       <a href={obj} download={f.name} />
                       {preview}
                     </Box>
                   </SwiperSlide>
-                );
-              })
+                  )
+                })
             }
           </Swiper>
         </ModalBody>
         <ModalFooter>
           <Button onClick={dc.onClose}>{t('closeButton')}</Button>
           <Button variant='ghost' onClick={() => {
-            Array.from(document.querySelectorAll('a[download]')).forEach((el) => el.click());
+            Array.from(document.querySelectorAll('a[download]')).forEach((el) => el.click())
           }}>{t('recieveButton')}</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
-  );
+  )
 }
